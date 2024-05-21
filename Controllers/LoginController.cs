e0a0a664a593a9d5434f202d5ad33e56c27a741a
eminegunes <emine.lalegns@gmail.com>
2024-05-22 00:46:05 +0300

    Controllers have been completed

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using TatilSeyahatWebSite.Models.classes;
+
+
+namespace TatilSeyahatWebSite.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        Context c = new Context();
+        public ActionResult Giris()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Giris(Admin admin)
+        {
+            var bilgiler = c.Admins.FirstOrDefault(x => x.Kullanici == admin.Kullanici && x.Sifre == admin.Sifre);
+            if (bilgiler != null)
+            {
+                FormsAuthentication.SetAuthCookie(bilgiler.Kullanici, false);
+                Session["Kullaici"] = bilgiler.Kullanici.ToString();
+                return RedirectToAction("Index", "Admin");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ActionResult LogOut()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Giris", "Login");
+        }
+    }
+}
